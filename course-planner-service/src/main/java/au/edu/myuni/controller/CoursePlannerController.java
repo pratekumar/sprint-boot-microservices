ae4b1d0903141d21c09a292794a9250ae550735f
Prateek Kumar <pkumar2@deloitte.com.au>
2019-07-27 11:48:30 +1000

    Formatting the files

--- course-planner-service/src/main/java/au/edu/myuni/controller/CoursePlannerController.java
+++ course-planner-service/src/main/java/au/edu/myuni/controller/CoursePlannerController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CoursePlannerController {
 
 	@RequestMapping("/courses")
-    public String getCourses() {
-        return "Getting all the courses....";
-
-    }
+	public String getCourses() {
+		return "Getting all the courses....";
+	}
 }
