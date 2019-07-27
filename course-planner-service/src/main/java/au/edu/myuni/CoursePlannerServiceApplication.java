384f98c483e4fb60c4ccc86d1b166eeeda515dab
Prateek Kumar <pkumar2@deloitte.com.au>
2019-07-27 11:45:53 +1000

    Prateek: First commit of basic course planner microservice using spring boot

---
+++ course-planner-service/src/main/java/au/edu/myuni/CoursePlannerServiceApplication.java
@@ -0,0 +1,13 @@
+package au.edu.myuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlannerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlannerServiceApplication.class, args);
+	}
+
+}
---
+++ course-planner-service/src/main/java/au/edu/myuni/controller/CoursePlannerController.java
@@ -0,0 +1,14 @@
+package au.edu.myuni.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoursePlannerController {
+
+	@RequestMapping("/courses")
+    public String getCourses() {
+        return "Getting all the courses....";
+
+    }
+}
